0b55716083b157549d25510552cb07d61f0e6dee
jawacaser <97850241+jawacaser@users.noreply.github.com>
2022-06-14 21:10:10 -0500

    Add passport and express-session

--- server.js
+++ server.js
@@ -3,18 +3,40 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const session = require('express-session');
+const passport = require('passport');
+const ObjectID = require('mongodb').ObjectID;
 
 const app = express();
 app.set('view engine', 'pug')
 
-
 fccTesting(app); //For FCC testing purposes
+
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(( user, done ) => {
+  done(null, user._id)
+});
+
+// passport.deserializeUser((id, done) => {
+//   myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+//     done(null, null);
+//   });
+// });
+
 app.route('/').get((req, res) => {
-  res.render('pug');
+  res.render('pug', { title: 'Hello', message: 'Please login' });
 });
 
 const PORT = process.env.PORT || 3000;
